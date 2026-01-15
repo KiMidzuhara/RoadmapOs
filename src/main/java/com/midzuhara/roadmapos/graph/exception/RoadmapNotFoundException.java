4ec99303deb1ae09f908fbcd4e6998159269c9f2
YugureKaze <yotsume52@gmail.com>
2026-01-15 19:04:53 +0300

    feat(graph): add RoadmapNotFoundException class

---
+++ src/main/java/com/midzuhara/roadmapos/graph/exception/RoadmapNotFoundException.java
@@ -0,0 +1,7 @@
+package com.midzuhara.roadmapos.graph.exception;
+
+public class RoadmapNotFoundException extends RuntimeException {
+    public RoadmapNotFoundException(String message) {
+        super(message);
+    }
+}
