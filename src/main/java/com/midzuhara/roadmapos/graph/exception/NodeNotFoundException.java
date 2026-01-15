85c905b69bed2f59aaea829a3bc62651d2b627cc
YugureKaze <yotsume52@gmail.com>
2026-01-15 21:16:24 +0300

    feat(graph): add NodeNotFoundException class

---
+++ src/main/java/com/midzuhara/roadmapos/graph/exception/NodeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.midzuhara.roadmapos.graph.exception;
+
+public class NodeNotFoundException extends RuntimeException {
+    public NodeNotFoundException(String message) {
+        super(message);
+    }
+}
