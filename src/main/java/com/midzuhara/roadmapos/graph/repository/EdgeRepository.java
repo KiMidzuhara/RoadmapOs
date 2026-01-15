74b66cf61e9d829b34105dc38805938981d07cdb
YugureKaze <yotsume52@gmail.com>
2026-01-15 18:01:09 +0300

    feat(graph): add EdgeRepository interface

---
+++ src/main/java/com/midzuhara/roadmapos/graph/repository/EdgeRepository.java
@@ -0,0 +1,13 @@
+package com.midzuhara.roadmapos.graph.repository;
+
+import com.midzuhara.roadmapos.graph.entity.Edge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EdgeRepository extends JpaRepository<Edge, Long> {
+
+    boolean existsBySourceNodeIdAndTargetNodeId(Long sourceNodeId, Long targetNodeId);
+
+    List<Edge> findAllByRoadmapId(Long roadmapId);
+}
